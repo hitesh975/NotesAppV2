1dced5c21059dbe0d97a8df96f73b292d6ae4419
Hitesh <HiteshNachankar@gmail.com>
2026-02-24 18:27:00 +0530

    added go back button

--- NotesApp/src/pages/viewNotes.tsx
+++ NotesApp/src/pages/viewNotes.tsx
@@ -1,10 +1,15 @@
+import ButtonType1 from "../components/Buttons";
+import { useNavigate } from "react-router-dom";
+
 export default function ViewNotes() {
+    const navigate = useNavigate()
     return(
         <div className="viewNotes">
             <h2>View Notes</h2>
             <div className="notesList">
-                {/* Notes will be displayed here */}
+                
             </div>
+            <ButtonType1 text="go back" onClick={() => navigate("/")} />
         </div>
     )
 }
