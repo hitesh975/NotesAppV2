b206dd169163fa849ff82ed64c338265d8f3549f
Hitesh <HiteshNachankar@gmail.com>
2026-02-24 18:06:25 +0530

    added view notes page, not functional yet, im tired

--- NotesApp/src/pages/home.tsx
+++ NotesApp/src/pages/home.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     return(
         <div>
             <ButtonsType1 text="Add Notes" onClick={() => navigate("/addNotes")}/>
+            <ButtonsType1 text="View Notes" onClick={() => navigate("/viewNotes")}/>
         </div>
     )
 }
---
+++ NotesApp/src/pages/viewNotes.tsx
@@ -0,0 +1,10 @@
+export default function ViewNotes() {
+    return(
+        <div className="viewNotes">
+            <h2>View Notes</h2>
+            <div className="notesList">
+                {/* Notes will be displayed here */}
+            </div>
+        </div>
+    )
+}
