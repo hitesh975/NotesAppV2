1635b48c8f227ad700b91773aaa74b1033370f6a
Hitesh <HiteshNachankar@gmail.com>
2026-03-16 16:28:48 +0530

    added formula search, UI update heavily needed for formula section

--- NotesApp/src/pages/viewFormulae.tsx
+++ NotesApp/src/pages/viewFormulae.tsx
@@ -4,6 +4,7 @@ import "./viewFormulae.css"
 import ButtonsType1 from "../components/Buttons/ButtonsType1";
 
 export default function ViewForumulae() {
+    const [search, setSearch] = useState('');
     const [formulae, setFormulae] = useState(()=>{
         return JSON.parse(localStorage.getItem('formulae') || '[]')
     })
@@ -11,11 +12,20 @@ export default function ViewForumulae() {
 
     return(
         <div className="container">
-            {formulae.map((Formula: string) => (
+            <input type="text"
+                   value={search}
+                   onChange={(e)=>(setSearch(e.target.value))} />
+            {formulae.map((Formula: string) => {
+                if (!Formula.toLowerCase().includes(search.toLowerCase())) {
+                            return null;
+                        }
+                return(
+                
                 <div key={Formula} className="formulae">
                     {Formula}
                 </div>
-            ))}
+            )
+            })}
             <ButtonsType1 text='Back' onClick={()=>(Navigate(-1))}></ButtonsType1>
         </div>
     )
